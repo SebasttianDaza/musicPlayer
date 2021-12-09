b01d41672c4b64ac760d769fa7dd89a9e7a4a64f
Sebastian Daza <jsebastiandaza2002@gmail.com>
2021-12-09 16:34:00 -0500

    Añadimos el menu de navegacion entre clases y otras funciones con JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+// Importar matriz de dados
+
+import { song } from './matrizMusic.js';
+
 //*Obtener los elementos del DOM
 
 const img = document.getElementById('img');
@@ -5,15 +9,60 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
+const listSongs = document.getElementById('songs');
 
-//
-const url_API = 'https://api.napster.com/v2.2/';
-const API = 'YzZhNDIyZTktZTgyNy00MDEwLWI4NTMtMTFmM2Q4MjU0NDdj';
-const getData = async () => {
-     fetch('https://api.napster.com/v2.2/', Headers = { apikey : API })
-     .then(response => response.json())
-     .then(data => {console.log(data)})
+// *Botones de reproduccion
+
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Estado de la musica
+
+let isPlaying = null;
+
+// Evento de los botones
+
+playBtn.addEventListener('click', () => {
+     playMusic();
+})
+
+// Funcion para cargar cancion
+
+function loadSong(){
+     //*Realizamos un ciclo
+     song.forEach((song, index) => {
+          //*Cargamos la plantillas
+          document.getElementById('songs').innerHTML += loadSong2(song);
+          //*Cargamos eventos
+          document.getElementById('persona2').addEventListener('click', () => {
+               playSong(index);
+          });
+     });
 }
 
-getData();
 
+// Funcion para cargar plantilla
+
+function loadSong2(song){
+     //*Plantilla
+     let plantilla = `
+          <li> <a id='persona2' href="#">${song.name}</a> </li>
+     `;
+
+     return plantilla;
+}
+
+//Funcion para reproducir cancion
+
+function playSong(index){
+     if(index !== isPlaying){
+          //*Falta funcion de cambio de clase
+          isPlaying = index;
+
+          audio.src = './audio' + song[index].cover;
+          audio.play();
+     }
+}
+
+loadSong();
