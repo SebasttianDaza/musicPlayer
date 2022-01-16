85d997d5bbf9892e654c7928d580a36533ecc593
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-01-15 23:13:40 -0500

    Add funcionality play audio
    I worked in the funcionalities for each a Json with its names and other more

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Changing in the player:
 - Interaction with API
 - Search with API 💯💯
   
-![Imagen de Web Site](https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/musicPlayer.png?alt=media&token=f0ed6788-b113-4aaf-b15a-607625f2380e)
+![Imagen de Web Site](https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/media.jpg?alt=media&token=7bcd1f8b-0054-4d8c-9038-2735c03e642e)
 
 [Music Player 🎶🎶🎶🎶](https://emprendeyourlifestyle.github.io/musicPlayer/)
 
--- src/app.js
+++ src/app.js
@@ -1,91 +1,43 @@
-// Importar matriz de dados
+// Change elements a variables
 
-import { song } from './matrizMusic.js';
-
-/**
- * //*Get to elements the music player
- * @param {img} Variable que contiene el elemento img para el reproductor
- * @param {title} Variable que contiene el elemento h3 para el titulo
- * @param {artist} Variable que contiene el elemento h4 para el artista
- * @param {progress} Variable que contiene el elemento para la barra de progreso
- * @param {listSongs} Variable que contiene el elemento ul para la lista de canciones
- * */
-
-const img = document.getElementById('img');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-let audios ;
-const progress = document.getElementById('progress');
-const listSongs = document.getElementById('songs');
-
-// *Controls de musicPlayer
-/**
- * //*Controls de musicPlayer
- * @param {playBtn} Variable que contiene el elemento para el boton play
- * @param {prevBtn} Variable que contiene el elemento para el boton prev
- * @param {nextBtn} Variable que contiene el elemento para el boton next
- */
-
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
-
-//Estado de la musica
-
-let isPlaying = null;
-
-//* Recuperar template
-
-const fragment = document.createDocumentFragment();
-
-// Evento de los botones
-
-playBtn.addEventListener('click', () => {
-     
-})
-
-// Funcion para cargar cancion
-
-function loadSong(){
-     //*Realizamos un ciclo
-     song.forEach((song, index) => {
-          //*Cargamos el template
-          listSongs.innerHTML += template(song, index);
-          
-     });
-
-     // Funcion para cargar plantilla
+const listSongs = document.querySelectorAll('#change li');
+const save = document.getElementById('guardar');
+const prev = document.getElementById('prev');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
 
 
+// Leer documnento Json en Js
 
+function requestJson () {
+     fetch('src/song.json')
+          .then( response => response.json())
+          .then( data => {
+               console.log(data);
+               changeSong(data);
+               //Evento para cargar cancion
+               listSongs.forEach( elements => {
+                    elements.addEventListener('click', () => {
+                         playSong(data);
+                    });
+               })
+          })
 }
 
-function template(song, index) {
-      let template = 
-      ` <li class='templa'> <a onclick='${playSong(index)}' class='link' href="#">${song.name}</a> </li>`;
-
-      return template
-
-     
-}
-
-
-
-//Funcion para reproducir cancion
-
-function playSong(index){
-     
-     if(index !== isPlaying){
-          isPlaying = index;
-          audios = new Audio(`../audio/Peaches.mp3`);
-          audios.autoPlay = true;
-          audios.play();
-          
+// Funcion para caragar Canciones
+function changeSong (data = []) {
+     console.log(data);
+     // Recorrer canciones
+     for (let i = 0; i < data.length; i++) {
+          listSongs[i].innerHTML = data[i].name;
      }
-     audios.play();
-
 }
 
-//*Funcion para cargar cancion llamada
+// Agregar events
 
-loadSong();
+const playSong = (data = []) => {
+     const audio = new Audio(`assets/audio/${data[0].url}`);
+     audio.play();
+}
+
+requestJson();
