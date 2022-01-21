08ab8b3c6f29fcc92c726d7fc367f7b3da6f2c51
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-01-21 17:54:55 -0500

    Song reproducing
    The song is reproducing with change class to distint between active and no-active

--- src/app.js
+++ src/app.js
@@ -8,45 +8,64 @@ const next = document.getElementById('next');
 
 
 // Leer documnento Json en Js
+document.addEventListener("click", (e) => {
+     if ( e.target.matches("#change li")){
+          
+     }
+})
+
 
 function requestJson () {
      fetch('src/song.json')
           .then( response => response.json())
           .then( data => {
-               console.log(data);
                changeSong(data);
                //Evento para cargar cancion
-               listSongs.forEach( elements => {
-                    elements.addEventListener('click', () => {
-                         playSong(data);
-                    });
-               })
+               playSong(data);
           })
 }
 
 // Funcion para caragar Canciones
 function changeSong (data = []) {
-     console.log(data);
      // Recorrer canciones
      for (let i = 0; i < data.length; i++) {
           listSongs[i].innerHTML = data[i].name;
      }
 }
 
+let actualSong = null;
 // Agregar events
 
 const playSong = (data = []) => {
      const audio = new Audio();
      for(let i = 0; i < data.length; i++) {
-          listSongs[i].addEventListener('click', () => {
+          listSongs[i].addEventListener('click', (e) => {
+               e.stopPropagation();
                audio.src = `assets/audio/${data[i].url}`;
-               if(audio.play === true) {
-                    audio.pause();
-               } else {
+               if ( i !== actualSong ) {
+                    changeClass(actualSong, e);
                     audio.play();
+                    actualSong = i;
+                    
+
                }
           })
      }
 }
+
+// Cambio de clases
+
+ const  changeClass = (actualSong, e) => {
+     if (actualSong === null) {
+       e.target.classList.remove("secundarios");
+       e.target.classList.add("primero");
+     } else if (actualSong !== null) {
+       listSongs[actualSong].classList.remove("primero");
+       listSongs[actualSong].classList.add("secundarios");
+       e.target.classList.remove("secundarios");
+       e.target.classList.add("primero");
+     }
+ }
+
 
 requestJson();
